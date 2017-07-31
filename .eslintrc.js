06cb27199bf56097057efb030e9c98b546990d41
Wilk Maia <wilkmaia@gmail.com>
2017-07-31 15:31:58 -0300

    Initial setup
    * Add needed packages
    * Add eslint setup for the project. We follow all eslint standard rules
    but the following:
     + Comma-dangle always multiline;
     + No unecessary semicolons;
     + Only single quotes.

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'extends': 'standard',
+    'rules': {
+        'semi': ['error', 'never'],
+        'quotes': ['error', 'single'],
+        'comma-dangle': ['error', 'always-multiline'],
+    }
+};
